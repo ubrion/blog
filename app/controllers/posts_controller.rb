c9c7cc151ad3efe4ac55faaee67bc65c72ce254a
Ubrion <ubrion.com@gmail.com>
2020-12-15 09:35:28 -0600

    feat: order posts by date desc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authorize, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.order(date: :desc)
   end
 
   def show
